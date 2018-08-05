b5be5c9afe4347e4ccf87ab080111c43dc5699c4
Luan Nguyen <nguyenluancntt7@gmail.com>
2018-08-05 14:47:42 +0700

    section 8

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component{
@@ -18,9 +18,9 @@ class AlbumList extends Component{
     render (){
         console.log(this.state.albums)
         return (
-            <View>
+            <ScrollView>
                {this.renderAlbum()}
-            </View>
+            </ScrollView>
         );
     }
 }
